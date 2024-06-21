a83f1e76283f6b86ee9deb8afe084777718a9ead
Yair292 <dluquep@unjbg.edu.pe>
2024-06-20 21:53:01 -0500

    Se agregó funciones al Ejercicio 1.

--- valor de pi.cpp
+++ valor de pi.cpp
@@ -2,16 +2,22 @@
 #include <cmath>
 #include <windows.h>
 using namespace std;
+float  radio;
+//funcion double
 double area(int x){
 	double pi=M_PI;
-	float area ;
+	float area;
 	area = pi * pow(x,2);
 	return area; 
 }
-int main(){
+//funcion void
+void escribir(){
 	SetConsoleOutputCP(CP_UTF8);
-	float  radio;
 	cout<<"Digite el radio para conseguir el área:\n"; cin>>radio;
 	cout<<"El area del circulo es "<<area(radio);
+}
+//funcion principal
+int main(){
+	escribir();
 	return 0;
 }
