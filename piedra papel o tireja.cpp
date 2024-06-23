466077592705af9e67d18a32e0430bd4c9afd452
Yair292 <dluquep@unjbg.edu.pe>
2024-06-22 19:15:29 -0500

    Se mejoro la solucion a interfaz

--- piedra papel o tireja.cpp
+++ piedra papel o tireja.cpp
@@ -2,39 +2,76 @@
 #include <cstdlib>
 #include <ctime>
 #include <windows.h>
+#include <conio.h>
+#include <unistd.h>
 using namespace std;
-
+//funcion principal
 int main() {
 	SetConsoleOutputCP(CP_UTF8);
+    string pc[] = {"PIEDRA", " PAPEL", "TIJERA"};
 	int selecciona, numero,cont_1=0,cont_2=0;
-	cout<<"|-------------------JUEGO PIEDRA,PAPEL-------------------|";
+	string Titulo = "|-------------------JUEGO PIEDRA,PAPEL O TIJERA -------------------|";
+	for (char c : Titulo) {
+        cout << c;
+        usleep(10000);
+    }
+    sleep(1);
 
 do{
 	srand(time(0));
 	
-    cout<<("\nSelecciona una opcion: |1. Piedra | 2. Papel 3. | Tijera |\n");
+    cout<<("\n         Selecciona una opcion: |1. Piedra | 2. Papel  | 3. Tijera |\n");
     cin >> selecciona;
-	numero = (rand()%2)+1;
+    string cerebroPC="|..................Pc Analizando las posibilidades.................|\n";
+    for (char a : cerebroPC) {
+        cout << a;
+        usleep(10000);
+    }
+    sleep(1);
+    	numero = (rand()%3)+1;
+    if (numero==1){
+    	cout<<"|......................El CPU HA ESCOGIDO "<<pc[0]<<"...................|\n"<<endl;
+	}else if(numero ==2){
+		cout<<"|......................El CPU HA ESCOGIDO "<<pc[1]<<"...................|\n"<<endl;
+	}else if(numero ==3){
+		cout<<"|......................El CPU HA ESCOGIDO "<<pc[2]<<"...................|\n"<<endl;
+	}
+    sleep(1);
 	//numero es la PC
 	if(selecciona==2 && numero==1) {
-		cout << "|-------------------------GANASTE------------------------|";
+		cout << "|=============================== GANASTE ==========================|\n";
 		cont_1++;
-	} else if(selecciona==3 && numero==2) {
-		cout << "|-------------------------GANASTE------------------------|";
+    }else if(selecciona==3 && numero==2){
+		cout << "|=============================== GANASTE ==========================|\n";
 		cont_1++;
-	} else if(selecciona==1 && numero==3) {
-		cout << "|-------------------------GANASTE------------------------|";
+    }else if(selecciona==1 && numero==3){
+		cout << "|=============================== GANASTE ==========================|\n";
 		cont_1++;
-	} else if(selecciona==numero){
-		cout << "|-------------------------EMPATE-------------------------|";
+	}else if(selecciona==numero){
+		cout << "|=============================== EMPATE ===========================|\n";
 	} else {
-    	cout << "|-------------------------PERDISTE------------------------|";
+    	cout << "|=============================== PERDISTE =========================|\n";
     	cont_2++;
 	}
-	cout<<endl<<"|---------------------------"<<cont_1<<"-"<<cont_2<<"---------------------------|";
+	cout<<endl<<"|----------------------------------"<<cont_1<<"-"<<cont_2<<"-----------------------------|";
 	if (cont_1 == 2|| cont_2== 2){
-		cout<<"\n|-----------------------DEFINICIÓN------------------------|"<<endl;
+		sleep(1);
+		system("cls");
+		cout<<"\n __________________________________________________________________ ";
+		cout<<"\n|                               ¡¡DEFINICIÓN!!                     |";
+		cout<<"\n|__________________________________________________________________|"<<endl;
+		sleep(2);
 	}
 }while(cont_1 <3 && cont_2<3);
+if(cont_1==3){
+    cout<<"\n _________________________________________________________________ ";
+    cout<<"\n|                           GANADOR!!!! JUGADOR                   |";
+    cout<<"\n|_________________________________________________________________|"<<endl;
+}else{
+    cout<<"\n __________________________________________________________________ ";
+    cout<<"\n|                              GANADOR!!!! CPU                     |";
+    cout<<"\n|__________________________________________________________________|"<<endl;
+}
+    getch();
 	return 0;
 }
